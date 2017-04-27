0797bcc0d49220b38f9b37fa4ef2e6218c8a8cf4
secheverria <sebastian.echeverria@gmail.com>
2017-04-27 13:01:54 -0300

    Simplified to use PSK for now.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,7 +4,9 @@
 
 import COSE.AlgorithmID;
 import COSE.CoseException;
+import COSE.KeyKeys;
 import COSE.OneKey;
+import com.upokecenter.cbor.CBORObject;
 import se.sics.ace.*;
 import se.sics.ace.as.DBConnector;
 import se.sics.ace.coap.as.CoapDBConnector;
@@ -17,9 +19,12 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Base64;
 
 
 public class Test {
+    static byte[] key256 = {'a', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,28, 29, 30, 31, 32};
+
     public static void main(String[] args)
     {
         try {
@@ -40,24 +45,26 @@ public class Test {
         creator.createUser(rootPwd);
         creator.createDBAndTables(rootPwd);
 
-        Set<String> keyTypes = new HashSet<>();
-        keyTypes.add("PSK");
-        keyTypes.add("RPK");
-
         OneKey asKey = OneKey.generateKey(AlgorithmID.ECDSA_256);
         CoapDBConnector dbCon = new CoapDBConnector(PostgreSQLDBCreator.DEFAULT_DB_URL, userName, userPwd);
         CoapsAS authorizationServer = new CoapsAS("myid", dbCon,
-                KissPDP.getInstance("src/main/resources/acl.json", dbCon), new KissTime(), asKey);
+                KissPDP.getInstance("src/main/resources/acl.json", dbCon), new KissTime(), null);
 
         Set<String> profiles = new HashSet<>();
         profiles.add("coap_dtls");
-        keyTypes.clear();
-        keyTypes.add("RPK");
+        Set<String> keyTypes = new HashSet<>();
+        keyTypes.add("PSK");
+        //keyTypes.add("RPK");
 
-        OneKey key = OneKey.generateKey(AlgorithmID.ECDSA_256);
-        OneKey publicKey = key.PublicKey();
-        dbCon.addClient("clientA", profiles, null, null, keyTypes, null,
-                publicKey);
+        CBORObject keyData = CBORObject.NewMap();
+        keyData.Add(KeyKeys.KeyType.AsCBOR(), KeyKeys.KeyType_Octet);
+        keyData.Add(KeyKeys.Octet_K.AsCBOR(), CBORObject.FromObject(key256));
+        OneKey sharedKey = new OneKey(keyData);
+
+        //String publicKeyStr = "piJYICg7PY0o/6Wf5ctUBBKnUPqN+jT22mm82mhADWecE0foI1ghAKQ7qn7SL/Jpm6YspJmTWbFG8GWpXE5GAXzSXrialK0pAyYBAiFYIBLW6MTSj4MRClfSUzc8rVLwG8RH5Ak1QfZDs4XhecEQIAE=";
+        //OneKey publickey = new OneKey(CBORObject.DecodeFromBytes(Base64.getDecoder().decode(publicKeyStr)));
+        dbCon.addClient("clientA", profiles, null, null, keyTypes, sharedKey,
+                null);
 
         System.out.println("Starting server");
         authorizationServer.start();
