3eb3f3add6bb41d8493f9e1a3ee8e0919f45d2b1
secheverria <sebastian.echeverria@gmail.com>
2017-05-19 16:52:08 -0400

    Temp test key.

--- src/main/java/AuthorizationServer.java
+++ src/main/java/AuthorizationServer.java
@@ -22,6 +22,7 @@ import java.util.Set;
 public class AuthorizationServer {
     private static byte[] sharedKey256Bytes = {'a', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
             20, 21, 22, 23, 24, 25, 26, 27,28, 29, 30, 31, 32};
+    //private static byte[] sharedKey128Bytes = {'a', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     private OneKey sharedKey;
 
     private static long howLongTokensLast = 1000000L;
